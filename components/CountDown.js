aaf1ff4f5107550ac94a2c5337bf4bb0575b7912
jangbersahaja <mmuter4@gmail.com>
2024-04-11 15:18:56 +0800

    repair background 2

--- components/CountDown.js
+++ components/CountDown.js
@@ -9,7 +9,7 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
 
-  height: 380px;
+  height: 330px;
 
   background: linear-gradient(90deg, #A65B76, #933B57, #933B57, #A65B76);
 
